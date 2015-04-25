2a3ca3b85ab59e753a05aaa2bf445337f2391230
Jacob Ford <j@cobford.com>
2015-04-25 17:24:25 -0400

    copied/pasted from Smashing Mag

--- dtuc-vote.php
+++ dtuc-vote.php
@@ -7,4 +7,46 @@
    Author: Jacob Ford
    Author URI: http://jacobford.com
    */
+
+add_action("wp_ajax_my_user_vote", "my_user_vote");
+add_action("wp_ajax_nopriv_my_user_vote", "my_must_login");
+
+function my_user_vote() {
+
+   if ( !wp_verify_nonce( $_REQUEST['nonce'], "my_user_vote_nonce")) {
+      exit("No naughty business please");
+   }   
+
+   $vote_count = get_post_meta($_REQUEST["post_id"], "votes", true);
+   $vote_count = ($vote_count == '') ? 0 : $vote_count;
+   $new_vote_count = $vote_count + 1;
+
+   $vote = update_post_meta($_REQUEST["post_id"], "votes", $new_vote_count);
+
+   if($vote === false) {
+      $result['type'] = "error";
+      $result['vote_count'] = $vote_count;
+   }
+   else {
+      $result['type'] = "success";
+      $result['vote_count'] = $new_vote_count;
+   }
+
+   if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+      $result = json_encode($result);
+      echo $result;
+   }
+   else {
+      header("Location: ".$_SERVER["HTTP_REFERER"]);
+   }
+
+   die();
+
+}
+
+function my_must_login() {
+   echo "You must log in to vote";
+   die();
+}
+
 ?>
