7ee04a5e11688a8c2783eb3e39a2dc79f3e2d82c
Jacob Ford <j@cobford.com>
2015-04-25 11:48:26 -0400

    WP Plugin Meta info added

---
+++ dtuc-vote.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: DtUC Vote
+   Plugin URI: http://designingtheurbancommons.org
+   Description: ballot counter for Designing the Urban Commons voting system
+   Version: 0.1
+   Author: Jacob Ford
+   Author URI: http://jacobford.com
+   License: Barely any
+   */
+?>
