377dd9c0228afcfcdd9bb980d616bd4d40d1f897
Jacob Ford <j@cobford.com>
2015-04-25 18:10:33 -0400

    copy/pasted script content from SM

---
+++ dtuc_vote_script.js
@@ -0,0 +1,24 @@
+jQuery(document).ready( function() {
+
+   jQuery(".user_vote").click( function() {
+      post_id = jQuery(this).attr("data-post_id")
+      nonce = jQuery(this).attr("data-nonce")
+
+      jQuery.ajax({
+         type : "post",
+         dataType : "json",
+         url : myAjax.ajaxurl,
+         data : {action: "my_user_vote", post_id : post_id, nonce: nonce},
+         success: function(response) {
+            if(response.type == "success") {
+               jQuery("#vote_counter").html(response.vote_count)
+            }
+            else {
+               alert("Your vote could not be added")
+            }
+         }
+      })   
+
+   })
+
+})
