be809343ebd5207920f9b92d4b7489a4b68dbfe8
Jacob Ford <j@cobford.com>
2015-04-25 18:12:10 -0400

    changing of a few terms

--- dtuc_vote_script.js
+++ dtuc_vote_script.js
@@ -8,13 +8,13 @@ jQuery(document).ready( function() {
          type : "post",
          dataType : "json",
          url : myAjax.ajaxurl,
-         data : {action: "my_user_vote", post_id : post_id, nonce: nonce},
+         data : {action: "dtuc_vote", post_id : post_id, nonce: nonce},
          success: function(response) {
             if(response.type == "success") {
                jQuery("#vote_counter").html(response.vote_count)
             }
             else {
-               alert("Your vote could not be added")
+               alert("Something’s gone terrible wrong")
             }
          }
       })   
