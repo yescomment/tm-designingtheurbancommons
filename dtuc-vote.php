e9d45a25600454eb324a8b976e52c8736a6a0c72
Jacob Ford <j@cobford.com>
2015-05-06 01:58:31 -0400

    will now also log logged-in vote attempts

--- dtuc-vote.php
+++ dtuc-vote.php
@@ -34,6 +34,7 @@ function loggedin_vote() {
    $result['error_message'] = "Hey! You work here! No voting for logged-in users.";
    $result = json_encode($result);
    echo $result;
+   log_vote("[BAD NONCE] Possible mischief from $ipaddress attempting to vote for" . $_REQUEST["post_id"]);
    die();
 
 }
