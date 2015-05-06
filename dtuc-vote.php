1939bb0cea46bfe526c97a3bfd32abad7cb1c001
Jacob Ford <j@cobford.com>
2015-05-06 02:12:32 -0400

    fixed newline in logger

--- dtuc-vote.php
+++ dtuc-vote.php
@@ -86,7 +86,7 @@ function log_vote($message) {
 
    $filename = plugin_dir_path( __FILE__ ) . 'dtuc_vote_log.txt';
    $timestamp = date("Y m d H:i:s", time());
-   $entry = $timestamp . '  ' . $message . /n;
+   $entry = $timestamp . '  ' . $message . "\r\n";
 
    file_put_contents($filename, "FILE_APPEND", $entry); // I think something about this is why it's not working.
 
